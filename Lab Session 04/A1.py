052191f29de28509832064bb20431447e456d9ea
GorleJayanth <jayanthgorle786@gmail.com>
2025-02-16 15:09:02 +0530

    A1.py

--- Lab Session 04/A1.py
+++ Lab Session 04/A1.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def load_data(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    return df
+
+# Example Usage
+file_path = "Lab Session Data.xlsx"
+sheet_name = "IRCTC Stock Price"
+df = load_data(file_path, sheet_name)
+print(df.head())  # Display first few rows
